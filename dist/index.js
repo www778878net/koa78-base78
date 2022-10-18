953ca4242858334b6b1e7781f927107bcf680afb
胡琤波 <657225485@qq.com>
2022-10-18 21:54:53 +0800

    export

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Validate78 = exports.Base78 = void 0;
 const Base78_1 = require("./dlldata/Base78");
+exports.Base78 = Base78_1.default;
 const Validate78_1 = require("./dlldata/Validate78");
-module.exports = {
-    Base78: Base78_1.default,
-    Validate78: Validate78_1.default
-};
+exports.Validate78 = Validate78_1.default;
 //# sourceMappingURL=index.js.map
