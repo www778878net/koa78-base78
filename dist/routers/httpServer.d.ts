8e21f4aec99e15189290ebbedf93c5c840ab53e6
frieda <657225485@qq.com>
2025-12-31 22:02:14 +0800

    console_handler = logging.StreamHandler()

--- dist/routers/httpServer.d.ts
+++ dist/routers/httpServer.d.ts
@@ -1,12 +1,6 @@
 /// <reference types="node" />
 import Koa from 'koa';
 import http from 'http';
-declare module 'koa' {
-    interface Request {
-        body?: any;
-        fields?: any;
-    }
-}
 export declare function startServer(port?: number): Promise<{
     app: Koa;
     httpServer: http.Server;
